ce147542fae44e8f709b7dddff3d7d80640b76de
Paulo Lafayette <pauloarthurlafayettecosta@gmail.com>
2025-08-27 22:05:07 -0300

    desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿using System.Globalization;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("123", "abc777", "111111111", 120);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("zap");
+
+
+Console.WriteLine("");
+
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone("777", "iphone x", "333333333", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Chess");
